c64e81febc6e8756941477be181c54c40548f774
Andreas Josefsson <andreas@barnensbibliotek.se>
2022-02-09 14:33:19 +0100

    2022-02-09 interface test

---
+++ interfaces/IPublicSearchCmdInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace KulturkatalogenDomain.Info
+{
+    public interface IPublicSearchCmdInfo
+    {
+        int arrTypID { get; set; }
+        string cmdTyp { get; set; }
+        string freeTextSearch { get; set; }
+        int konstartID { get; set; }
+        int startYear { get; set; }
+        int stoppYear { get; set; }
+    }
+}
---
+++ interfaces/IPublicSearchInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KulturkatalogenDomain.Info
+{
+    public interface IPublicSearchInfo
+    {
+        int? ArkivStatus { get; set; }
+        int? ArrangemangstypID { get; set; }
+        int ArrID { get; set; }
+        DateTime? Datum { get; set; }
+        string ImageUrl { get; set; }
+        string konstform { get; set; }
+        int? konstform2 { get; set; }
+        int? konstform3 { get; set; }
+        string Organisation { get; set; }
+        DateTime? periodslut { get; set; }
+        DateTime? periodstart { get; set; }
+        string Publicerad { get; set; }
+        string Rubrik { get; set; }
+        string startyear { get; set; }
+        string stoppyear { get; set; }
+        string Underrubrik { get; set; }
+        int? UtovarID { get; set; }
+    }
+}
